5b9affdcdb95eb252089ee639af2e8f177981d6b
OliverZott <zott_oliver@web.de>
2020-11-02 16:33:15 +0100

    Composer require nesbot/carbon

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ApiResource(
  *     collectionOperations={"get", "post"},
  *     itemOperations={
- *     "get"={"path"="/everybodylovescheese/{id}"},
+ *     "get"={},
  *     "put"
  *     },
  *     shortName="cheeses"
