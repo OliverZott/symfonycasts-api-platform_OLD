d5d28a18a96be351f5b2d9baf6d552af49d3b640
OliverZott <zott_oliver@web.de>
2020-11-16 16:12:07 +0100

    Adds serialization-groups for normalizer / denormalizer

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
@@ -15,6 +16,8 @@ use Doctrine\ORM\Mapping as ORM;
  *     "get"={},
  *     "put"
  *     },
+ *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="ReadModel"},
+ *     denormalizationContext={"groups"={"cheese_listing:write"}},
  *     shortName="cheeses"
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
@@ -30,11 +33,13 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"cheese_listing:read", "cheese_listing:write"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"cheese_listing:read"})
      */
     private $description;
 
@@ -42,6 +47,7 @@ class CheeseListing
      * Price in cents
      *
      * @ORM\Column(type="integer")
+     * @Groups({"cheese_listing:read", "cheese_listing:write"})
      */
     private $price;
 
@@ -53,7 +59,7 @@ class CheeseListing
     /**
      * @ORM\Column(type="boolean")
      */
-    private $isPublished;
+    private $isPublished = false;
 
     public function __construct()
     {
@@ -82,6 +88,18 @@ class CheeseListing
         return $this->description;
     }
 
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     * @return $this
+     * @Groups ({"cheese_listing:write"})
+     */
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
@@ -106,6 +124,11 @@ class CheeseListing
         return $this->createdAt;
     }
 
+    /**
+     * How long ago since cheese was added
+     * @return string
+     * @Groups ({"cheese_listing:read"})
+     */
     public function getCreatedAtAgo(): string {
         return Carbon::instance($this->getCreatedAt())->diffForHumans();
     }
